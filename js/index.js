27264cb1cee9c15953018209224075e875488cea
gracefuljs <32861177+gracefuljs@users.noreply.github.com>
2020-02-03 21:58:34 -0600

    Fixed uploading error

--- js/index.js
+++ js/index.js
@@ -4,9 +4,11 @@ window.onload = function(){
 		mapCtx     = tileMap.getContext("2d"),
 		charaCtx   = charaMap.getContext("2d")
 		width      = charaMap.width  = tileMap.width  = 900,
-		height     = charaMap.height = tileMap.height = 600,
-		tileWidth  = 60,
-		tileHeight = 30;
+		height     = charaMap.height = tileMap.height = 600;
+		
+	//Tiles are conventionally twice as wide as they are tall.
+	let	tileWidth  = 60,
+		tileHeight = 30; 
 
 
 	let grid = [
@@ -15,11 +17,15 @@ window.onload = function(){
 		[15,  8,  8,  8,  8,  8,  8,  8, 15, 15, 15, 15],
 		[15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15],
 		[15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15],
+		[15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15],
+		[15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15],
+		[15,  8,  8,  8,  8,  8,  8,  8,  8,  8,  8, 15],
 		[15,  1,  1,  1, 15, 15, 15, 15, 15, 15, 15, 15],
 		[15,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1, 15],
 		[15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15],
 	]
 
+	//Move the origin of the tile map to the center of the screen
 	mapCtx.translate(width / 4, 50);
 	charaCtx.translate(width / 4, 50);
 	
@@ -27,84 +33,31 @@ window.onload = function(){
 	img.addEventListener("load", () => {draw()});
 	img.src = "images/tileset.png";
 
+	//This particular render method draws the tiles in a diamond shape, where the grid looks like a standard cartesian grid only rotated.
 	function draw(){
 		for(let y = 0; y < grid.length; y ++){
 			
 			let row = grid[y];
 			for(let x = 0; x < row.length; x++){
 				drawImageTile(x, y, row[x]);
-				//drawBlock(x, y, Math.floor(Math.random() * 4));
 			}
 		}
 	}
 
 
+	function getTileCoords(x, y){
+		//To move to the next tile, we must move 1/2 tile width on the x axis, and 1/2 tile width on the y axis.
+		//Additionally, an isometric move has a cartesian x and y component, so moving along either isometric axis affects both cartesian x and y.
+		let tileX = (x - y) * (tileWidth / 2); //When moving along the x isometric axis the cartesian x increases and the y decreases.
+		let tileY = (x + y) * (tileHeight / 2); //When moving along the y isometric axis the cartesian x increases and the y increases.
+
+		return [ tileX, tileY ]
+	}
+
 	function drawImageTile(x, y, index){
-		mapCtx.save();
-		mapCtx.translate((x - y) * (tileWidth / 2), (x + y) * (tileHeight / 2) + (index < 4 ? 5: 0));
-		mapCtx.drawImage(img, index * tileWidth, 0, tileWidth, img.height, -tileWidth / 2, 0, tileWidth, img.height);
-		
+		let [tileX, tileY] = getTileCoords(x, y);
+		mapCtx.drawImage(img, index * tileWidth, 0, tileWidth, img.height, tileX, tileY + (index < 4 ? 5: 0), tileWidth, img.height);
 
-		mapCtx.restore();
 	};
-
-	function drawBlock(x, y, z){
-		let top   = "#eeeeee",
-			right = "#cccccc",
-			left  = "#999999";
-
-		mapCtx.save();
-		mapCtx.translate((x - y) * (tileWidth / 2), (x + y) * (tileHeight / 2));
-
-		//Draw Top
-		mapCtx.beginPath();
-		mapCtx.moveTo(0, -z * tileHeight);
-		mapCtx.lineTo(tileWidth / 2, tileHeight / 2 - (z * tileHeight));
-		mapCtx.lineTo(0, tileHeight - (z * tileHeight) );
-		mapCtx.lineTo(-tileWidth / 2, tileHeight / 2 - (z * tileHeight));
-		mapCtx.closePath();
-		mapCtx.fillStyle = top;
-		mapCtx.fill();
-
-		//Draw Left
-		mapCtx.beginPath();
-		mapCtx.moveTo(-tileWidth / 2, tileHeight / 2 - (z * tileHeight));
-		mapCtx.lineTo(0, tileHeight - (z * tileHeight));
-		mapCtx.lineTo(0, tileHeight);
-		mapCtx.lineTo(-tileWidth / 2, tileHeight / 2);
-		mapCtx.closePath();
-		mapCtx.fillStyle = left;
-		mapCtx.fill();
-
-		//Draw Right
-		mapCtx.beginPath();
-		mapCtx.moveTo(tileWidth / 2, tileHeight / 2 -( z * tileHeight));
-		mapCtx.lineTo(0, tileHeight - (z * tileHeight));
-		mapCtx.lineTo(0, tileHeight);
-		mapCtx.lineTo(tileWidth / 2, tileHeight / 2);
-		mapCtx.closePath();
-		mapCtx.fillStyle = right;
-		mapCtx.fill();
-
-		mapCtx.restore();
-	}
-
-	
-	function drawTile(x, y, color){
-		mapCtx.save();
-		mapCtx.translate((x - y) * (tileWidth / 2), (x + y) * (tileHeight / 2));
-
-		mapCtx.beginPath();
-		mapCtx.moveTo(0, 0);
-		mapCtx.lineTo(tileWidth / 2, tileHeight / 2);
-		mapCtx.lineTo(0, tileHeight);
-		mapCtx.lineTo(-tileWidth / 2, tileHeight / 2);
-		mapCtx.closePath();
-		mapCtx.fillStyle = color;
-		mapCtx.fill();
-
-		mapCtx.restore(); 
-	}
-
 
 }
